4ad6149eefa7891fa0320d54f7f575786ee0cd2b
chillipeppersmoothy <chillipeppersmoothy@users.noreply.github.com>
2024-08-12 06:20:17 +0000

    deploy: b55c6c0686d4020a8f5945915403153cfb5f7796

---
+++ assets/NotFound--mO6l4pY.js
@@ -0,0 +1,1 @@
+import{j as o,L as e}from"./index-40SuWMdA.js";const t=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Page Not Found"}),o.jsx(e,{to:"/Git-search/",children:" Go Back To Home Page"})]});export{t as default};
---
+++ assets/about-BSvKL52S.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-40SuWMdA.js";const o=()=>s.jsx("h1",{children:"About Us Page"});export{o as default};
